ef7be8b2278234d8ba14ac9221f708f2a02e0839
Brandon Keepers <brandon@opensoul.org>
2013-08-29 23:19:27 -0400

    fresh project

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global notifications, $*/
+
+
+window.notifications = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    notifications.init();
+});
