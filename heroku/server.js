55733b1b70cfb7269fbc480b7745cdf7069feb16
Brandon Keepers <brandon@opensoul.org>
2013-09-15 22:47:03 -0400

    use x-forwarded-proto header

--- heroku/server.js
+++ heroku/server.js
@@ -4,12 +4,17 @@ var app = express();
 function requireHTTPS(req, res, next) {
   res.setHeader('Strict-Transport-Security', 'max-age=31536000');
 
-  if (!req.secure) {
+  isSecure = req.secure || req.headers['x-forwarded-proto'] == 'https'
+
+  if (!isSecure) {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
 }
 
-app.use(requireHTTPS);
+if(process.env.NODE_ENV == 'production') {
+  app.use(requireHTTPS);
+}
+
 app.use(express.static(__dirname));
 app.listen(process.env.PORT || 3000);
