f02ffd1eb6f06dd02ef40630d0205362eda1e425
Brandon Keepers <brandon@opensoul.org>
2013-09-15 22:41:46 -0400

    Force HTTPS

--- heroku/server.js
+++ heroku/server.js
@@ -1,5 +1,15 @@
-'use strict';
+var express = require('express');
+var app = express();
 
-var statik = require('statik');
-var server = statik.createServer('.');
-server.listen();
+function requireHTTPS(req, res, next) {
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000');
+
+  if (!req.secure) {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+app.use(express.static(__dirname));
+app.listen(process.env.PORT || 3000);
